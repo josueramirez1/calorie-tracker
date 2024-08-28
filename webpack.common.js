997d59599adc65952b2553cbf5af2e361e6ed2b7
josueramirez1 <josue67ramirez@gmail.com>
2024-08-28 16:23:56 -0700

    changing files

--- webpack.common.js
+++ webpack.common.js
@@ -2,9 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    app: "./src/index.js",
-  },
+  entry: "./src/script.js",
   plugins: [
     new HtmlWebpackPlugin({
       title: "Production",
